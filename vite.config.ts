fc7db2c2cc56e9e88ca28278111a8ee8046e16db
anakon73 <nazarkinah73@gmail.com>
2022-12-14 15:16:02 +0200

    build: set up alias imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import Components from 'unplugin-vue-components/vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [Components({}), vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
