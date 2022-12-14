9fc7a710a765093573c3d40aed4f1644c5556383
anakon73 <nazarkinah73@gmail.com>
2022-12-14 16:39:09 +0200

    feat(router): setup router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,26 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import EventsList from '@/views/EventsList.vue'
+import AboutPage from '@/views/AboutPage.vue'
+import EventDetails from '@/views/EventDetails.vue'
 
-const routes = []
+const routes = [
+  {
+    path: '/',
+    name: 'EventsList',
+    component: EventsList,
+  },
+  {
+    path: '/about',
+    name: 'AboutPage',
+    component: AboutPage,
+  },
+  {
+    path: '/events/:id',
+    name: 'EventDetails',
+    props: true,
+    component: EventDetails,
+  },
+]
 
 const router = createRouter({
   history: createWebHashHistory(),
