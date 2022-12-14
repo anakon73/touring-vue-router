698fe74dbc3c9f6b058fadf03db6769f8dfb1bc8
anakon73 <nazarkinah73@gmail.com>
2022-12-14 16:29:48 +0200

    build(router): setup router

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div class="text-7xl">Hello world</div>
+  <div class="text-center text-slate-700 font-sans m-2">
+    <nav class="p-[30px]">
+      <router-link
+        class="font-bold text-slate-700"
+        active-class="text-emerald-400"
+        :to="{ name: 'EventsList' }"
+        >Events</router-link
+      >
+      |
+      <router-link
+        class="font-bold text-slate-700"
+        active-class="text-emerald-400"
+        :to="{ name: 'AboutPage' }"
+        >About</router-link
+      >
+    </nav>
+    <router-view />
+  </div>
 </template>
