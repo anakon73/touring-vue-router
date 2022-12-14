11b27991770148dfaca5fc759ad0c9ffdb606394
anakon73 <nazarkinah73@gmail.com>
2022-12-14 16:34:10 +0200

    feat(views/eventlist): create eventlist page

---
+++ src/types/event.ts
@@ -0,0 +1,11 @@
+export type event = {
+  id: string
+  category: string
+  title: string
+  description: string
+  location: string
+  date: string
+  time: string
+  petsAllowed: boolean
+  organizer: string
+}
---
+++ src/views/EventList.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import axios from 'axios'
+import type { event } from '@/types/event'
+const events = ref<event[]>([])
+axios
+  .get('https://my-json-server.typicode.com/anakon73/real-world-vue-3/events')
+  .then((response) => {
+    events.value = response.data
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+</script>
+
+<template>
+  <h1 class="mb-[18px] text-[32px] font-bold">Events For Good</h1>
+  <div class="flex flex-col items-center">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
