4cdb76116965bfdfed7ac52818bfcdb07ef07c49
anakon73 <nazarkinah73@gmail.com>
2022-12-23 16:38:54 +0200

    feat(views/event): added programmic navigation for register page

--- src/views/event/EventRegister.vue
+++ src/views/event/EventRegister.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts">
+import router from '@/router';
+
 const { event } = defineProps(['event'])
+
+const register = () => {
+  router.push({
+    name: "EventDetails",
+  })
+}
 </script>
 
 <template>
   <p class="my-5">Register for the event here</p>
+  <button @click="register">Register Me</button>
 </template>
